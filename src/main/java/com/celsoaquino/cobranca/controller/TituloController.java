ea4aae6b9db7719e88306cfe79bd1ecdbd93f31c
Celso Aquino <celso.aquino@ymail.com>
2020-03-31 14:27:37 -0300

    Montando o combo dinamicamente

--- src/main/java/com/celsoaquino/cobranca/controller/TituloController.java
+++ src/main/java/com/celsoaquino/cobranca/controller/TituloController.java
@@ -1,11 +1,19 @@
 package com.celsoaquino.cobranca.controller;
 
+import com.celsoaquino.cobranca.model.StatusTitulo;
 import com.celsoaquino.cobranca.model.Titulo;
 import com.celsoaquino.cobranca.repository.TituloRepoitory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -18,13 +26,22 @@ public class TituloController {
     }
 
     @GetMapping("/novo")
-    public String novo() {
-        return "CadastroTitulo";
+    public ModelAndView novo() {
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("todosStatusTitulo", StatusTitulo.values());
+        return mv;
     }
 
     @PostMapping
-    private String salvar(Titulo titulo){
+    private ModelAndView salvar(Titulo titulo){
         repoitory.save(titulo);
-        return "CadastroTitulo";
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("mensagem", "Titulo salvo com sucesso!");
+        return mv;
+    }
+
+    @ModelAttribute("allStatus")
+    public List<StatusTitulo> todosStatusTitulo(){
+        return Arrays.asList(StatusTitulo.values());
     }
 }
